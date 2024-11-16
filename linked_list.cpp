e45d6ef809f0d19400973c4f7a8b1bcfd093b43a
yuyang011 <142098542+yuyang011@users.noreply.github.com>
2024-11-17 05:34:05 +0800

    Add files via upload

---
+++ linked_list.cpp
@@ -0,0 +1,37 @@
+#include "linked_list.h"
+
+// Returns the value at head
+int List::head() {
+  // TODO: Implement this method
+}
+
+// Checks whether the container is empty
+bool List::empty() const {
+  // TODO: Implement this method
+}
+
+// Returns the number of elements
+size_t List::size() const {
+  // TODO: Implement this method
+}
+
+// Inserts an element to the head
+void List::push_head(int element) {
+  // TODO: Implement this method
+}
+
+// Removes the head element
+int List::pop_head() {
+  // TODO: Implement this method
+}
+
+// Checks whether the container contains the specified element
+bool List::contains(int element) const {
+  // TODO: Implement this method
+}
+
+// Returns a std::string equivalent of the container
+std::string List::to_string() const {
+  // TODO: Implement this method
+  return "";
+}
